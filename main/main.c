33687abccde08107e13d2fa7f36af62a0d9b390e
Sergio <svzinoviev@gmail.com>
2026-06-07 20:34:05 +0200

    Init display, clear its framebuffer, fix typo

--- main/main.c
+++ main/main.c
@@ -5,6 +5,7 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "st7789v_esp_driver.h"
+#include "string.h"
 
 // TODO: move pins to the board pins file
 #define BOARD_POWERON        15
@@ -19,11 +20,43 @@ void power_turn_on(bool is_on)
     gpio_set_level(BOARD_POWERON, is_on);
 }
 
+static SemaphoreHandle_t  s_done;
+
+bool lcd_transfer_is_done_cb(
+    esp_lcd_panel_io_handle_t io,
+    esp_lcd_panel_io_event_data_t *edata,
+    void *user_ctx)
+{
+    (void)io;
+    (void)edata;
+    (void)user_ctx;
+    BaseType_t hp_woken = pdFALSE;
+    xSemaphoreGiveFromISR(s_done, &hp_woken);
+    return hp_woken == pdTRUE;
+}
+
+static uint16_t *fb;
+#define WIDTH   320
+#define HEIGHT  170
+
 void app_main(void)
 {
     power_turn_on(true);
 
+    s_done = xSemaphoreCreateBinary();
+    assert(s_done);
+    xSemaphoreGive(s_done);
+
+    size_t fb_bytes = (size_t)WIDTH * HEIGHT * sizeof(uint16_t);
+    fb = heap_caps_malloc(fb_bytes, MALLOC_CAP_DMA);
+    assert(fb);
+    memset(fb, 0, fb_bytes);
+
+    st7789v_esp_driver_init(lcd_transfer_is_done_cb);
+
     while(true) {
         vTaskDelay(1);
+        st7789v_esp_driver_draw_bitmap(0, 0, WIDTH, HEIGHT, fb);
+        xSemaphoreTake(s_done, portMAX_DELAY);
     }
 }
--- main/st7789v_esp_driver.h
+++ main/st7789v_esp_driver.h
@@ -5,4 +5,4 @@
 int st7789v_esp_driver_init(
     esp_lcd_panel_io_color_trans_done_cb_t bus_transmission_complete_cb);
 void st7789v_esp_driver_draw_bitmap(uint16_t x, uint16_t y, uint16_t width,
-                                    uint16_t hight, uint16_t *data);
+                                    uint16_t height, uint16_t *data);
